e361628d89b93e45a55a022060ae3d045e36dd1a
Joao <jvictorsgon@gmail.com>
2025-02-02 09:25:38 -0300

    Primeiro commit

---
+++ src/main/java/com/joaoVictor/DomainORM_Lab/DomainOrmLabApplication.java
@@ -0,0 +1,13 @@
+package com.joaoVictor.DomainORM_Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainOrmLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainOrmLabApplication.class, args);
+	}
+
+}
